fa3b9638f7ecb86c9d7f6f5b6d60d9e61cd1d36d
Dave Dunkin <dave.dunkin@logos.com>
2023-09-08 13:53:30 -0700

    Update vscode dependencies. (#2)

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,22 @@
 import * as path from 'path';
-import { Trace } from 'vscode-jsonrpc';
 import { workspace, ExtensionContext } from 'vscode';
-import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 
-export function activate(context: ExtensionContext) {
+let client: LanguageClient;
+
+export async function activate(context: ExtensionContext) {
 	const serverDll = path.join(context.extensionPath, 'server', 'Facility.LanguageServer.dll');
-	////const serverDll = path.join(context.extensionPath, '..', 'FacilityLanguageServer', 'src', 'Facility.LanguageServer', 'bin', 'Debug', 'netcoreapp2.0', 'Facility.LanguageServer.dll');
+	// const serverDll = path.join(
+	// 	context.extensionPath,
+	// 	'..',
+	// 	'FacilityLanguageServer',
+	// 	'src',
+	// 	'Facility.LanguageServer',
+	// 	'bin',
+	// 	'Debug',
+	// 	'net6.0',
+	// 	'Facility.LanguageServer.dll'
+	// );
 	const serverOptions: ServerOptions = {
 		run: { command: 'dotnet', args: [serverDll, '-lsp'] },
 		debug: { command: 'dotnet', args: [serverDll, '-lsp'] },
@@ -23,11 +34,13 @@ export function activate(context: ExtensionContext) {
 		},
 	};
 
-	const client = new LanguageClient('languageServerFsd', 'Facility Service Definition', serverOptions, clientOptions);
-	client.trace = Trace.Verbose;
-	const clientRunScope = client.start();
+	client = new LanguageClient('languageServerFsd', 'Facility Service Definition', serverOptions, clientOptions);
+	await client.start();
+}
 
-	// Push the disposable to the context's subscriptions so that the
-	// client can be deactivated on extension deactivation
-	context.subscriptions.push(clientRunScope);
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
 }
